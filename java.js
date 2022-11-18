4b27cf9a0c58fac0a29e20cecff11750ef43290e
Matthew <matthewjcrosby1@gmail.com>
2022-11-18 18:17:30 +0700

    Added ability for players to add X and 0 to the board on click.  Still has countless bugs.

--- java.js
+++ java.js
@@ -9,23 +9,66 @@ const gameboard = (() =>
 return {gameboard};
 })();
 
-const player = (name, symbol) => {
+const boardDivs = document.querySelectorAll(".board-div");
+
+const player = (name, symbol, status) => {
     const wins = 0
     let playerSymbol = symbol
-    
-    return {name}
+    let playing = status
+    return {name, playing, playerSymbol}
 }
-const playerOne = player("PlayerOne", "X");
-const playerTwo = player("PlayerTwo", "O")
+const playerOne = player("PlayerOne", "X", true);
+const playerTwo = player("PlayerTwo", "O", false)
 
-const displayController = (() => {
-    const boardOnScreen = document.querySelectorAll(".board-div");
+const displayController = ((choice, player) => {
 
-    let choice = window.prompt(`${playerOne.name} make your choice`)
+    let playerSymbol = ""
+    let chosenDiv = ""
 
-    gameboard.gameboard.splice(choice, 0, "X")
-
-    for (let i = 0; i < boardOnScreen.length; i++) {
-        boardOnScreen[i].innerHTML = gameboard.gameboard[i]
+    const change = (choice,player) => {
+    playerSymbol = player.playerSymbol
+    chosenDiv = choice;
+    if (playerOne.playing == true) {
+        playerOne.playing = false
+        playerTwo.playing = true
+    } else {
+        playerTwo.playing = false
+        playerOne.playing = true
     }
-})()
+    updateboard();
+    }
+
+    function updateDisplay() {
+        for (let i = 0; i < boardDivs.length; i++) {
+        boardDivs[i].innerHTML = gameboard.gameboard[i]
+    }}
+
+    function updateboard() {
+        let arrayFrom = Array.from(boardDivs)
+        let boardSpot = arrayFrom.findIndex((element) => element == chosenDiv)
+        gameboard.gameboard.splice(boardSpot, 0, playerSymbol)
+        updateDisplay();
+    }
+    return {change};
+})();
+
+const game = (() => {
+    let player = ""
+    function getplayer() {
+    if (playerOne.playing == true) {
+        player = playerOne;
+    } else
+        player = playerTwo;
+}
+
+    boardDivs.forEach
+    (div => div.addEventListener("click", getChoice))
+
+
+    function getChoice() {
+       getplayer()
+       let divChosen = event.target
+       displayController.change(divChosen, player);
+    }
+    return {getChoice}
+})();
